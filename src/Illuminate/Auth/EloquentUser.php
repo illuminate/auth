bbfea8ff6625c1e710be32b361c7e44fe3483934
Taylor Otwell <taylorotwell@gmail.com>
2012-10-23 10:15:14 -0500

    added an EloquentUser base class.

---
+++ src/Illuminate/Auth/EloquentUser.php
@@ -0,0 +1,27 @@
+<?php namespace Illuminate\Auth;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EloquentUser extends Model implements UserInterface {
+
+	/**
+	 * Get the unique identifier for the user.
+	 *
+	 * @return mixed
+	 */
+	public function getAuthIdentifier()
+	{
+		return $this->getKey();
+	}
+
+	/**
+	 * Get the password for the user.
+	 *
+	 * @return string
+	 */
+	public function getAuthPassword()
+	{
+		return $this->password;
+	}
+
+}
